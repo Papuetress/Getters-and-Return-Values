b673ffd39de6ca8c057ec56763eef94cb63f8a19
Papuetress <papuetress@gmail.com>
2015-03-09 01:25:50 +0200

    Signed-off-by: Papuetress <papuetress@gmail.com>

--- src/looping2.java
+++ src/looping2.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 
 class Search {
 
-	void find(String g) {
-		String[] texts = { "cartof", "ceapa", "ghimbir", "gulie", "hasma",
+	void legumeFinder(String g) {
+		String[] legume = { "cartof", "ceapa", "ghimbir", "gulie", "hasma",
 				"sparanghel", "telină", "usturoi", "hrean", "morcov",
 				"pastarnac", "ridiche", "scortonera", "sfecla", "ardei",
 				"bamă", "castravete", "dovleac", "dovlecel", "pepene galben",
@@ -12,7 +12,23 @@ class Search {
 				"laptuca", "leustean", "loboda", "macris", "marar",
 				"patrunjel", "tarhon", "salata", "spanac", "stevie", "telina",
 				"varza", "fasole" };
-		if (Arrays.asList(texts).contains(g)) {
+		if (Arrays.asList(legume).contains(g)) {
+			System.out.println("Este ba sa moara Sandu Hingheru'");
+		} else {
+			System.out.println("Pffff, nu e sa moara Jean");
+		}
+	}
+
+	void fructeFinder(String g) {
+		String[] fructe = { "banane", "mere", "pere", "ananas", "avocado",
+				"alune", "castane", "capsuni", "cirese", "coacaze", "cocos",
+				"corcoduse", "dude", "fistic", "fragi", "grepfrut", "gutui",
+				"kiwi", "lamai", "pepene galben", "pepene verde", "mango",
+				"mandarine", "macese", "creson", "masline", "migdale", "mure",
+				"nuci", "nectarine", "papaya", "piersici", "portocale",
+				"piper", "prune", "rodii", "smochine", "soc", "visine",
+				"struguri", "zmeura" };
+		if (Arrays.asList(fructe).contains(g)) {
 			System.out.println("Este ba sa moara Sandu Hingheru'");
 		} else {
 			System.out.println("Pffff, nu e sa moara Jean");
@@ -25,17 +41,26 @@ public class looping2 {
 
 	public static void main(String[] args) {
 
+		Search bot = new Search();
 		boolean bool = true;
 		while (bool) {
 			scan = new Scanner(System.in);
-			System.out.println("Ia zi gogule o leguma, tasteaza 'quit' pentru terminare");
-			String g = scan.nextLine();
-			if (g.equals("quit")) {
+			System.out.println("Ia zi gogule 'legume' sau 'fructe'");
+			String input = scan.nextLine();
+			if (input.equals("quit")) {
 				bool = false;
 				System.out.println("Program Terminated");
+			} else if (input.equals("fructe")) {
+				System.out.println("Baga niste fructe");
+				String fructe = scan.nextLine();
+				bot.fructeFinder(fructe);
+			} else if (input.equals("legume")) {
+				System.out.println("Zi o leguma");
+				String legume = scan.nextLine();
+				bot.legumeFinder(legume);
 			} else {
-				Search bot = new Search();
-				bot.find(g);
+				System.out.println("Unknown command, type 'fructe' sau 'legume'"
+								+ "\n'quit' pt terminare");
 			}
 
 		}
